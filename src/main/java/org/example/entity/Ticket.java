dd1319011f8370e3e6276904ea3968152087bca2
Vladimir <karakisvladimir@gmail.com>
2023-12-15 23:31:46 +0200

    second 2

---
+++ src/main/java/org/example/entity/Ticket.java
@@ -0,0 +1,36 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "from_planet_id", nullable = false)
+    private Planet fromPlanet;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "to_planet_id", nullable = false)
+    private Planet toPlanet;
+
+}
