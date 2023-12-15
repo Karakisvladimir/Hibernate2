b7b71c9e243db3f7f68d9575bb4121d57ebc1b2a
Vladimir <karakisvladimir@gmail.com>
2023-12-15 23:47:32 +0200

    second 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.example.entity.Client;
 import org.example.entity.Planet;
 import org.example.entity.Ticket;
 import org.example.storage.DatabaseinitService;
+import org.hibernate.metamodel.spi.ManagedTypeRepresentationStrategy;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,48 +18,48 @@ public class Main {
 
                 new DatabaseinitService().initDb();
 
-//                ClientCrudService clientService = new ClientCrudService();
-//                PlanetCrudService planetCrudService = new PlanetCrudService();
-//                TicketCrudService ticketCrudService = new TicketCrudService();
-//
-//                Client clientOne = clientService.getClientById(1L);
-//                Client clientFive = clientService.getClientById(5L);
-//                Planet planetAlderaan = planetCrudService.getPlanetById("MARS");
-//                Planet planetNaboo = planetCrudService.getPlanetById("VENUS");
-//                Planet planetEndor = planetCrudService.getPlanetById("EARTH");
-//
-//                Ticket ticketOne = new Ticket();
-//                ticketOne.setCreatedAt(LocalDate.now());
-//                ticketOne.setClient(clientOne);
-//                ticketOne.setFromPlanet(planetAlderaan);
-//                ticketOne.setToPlanet(planetNaboo);
-//                clientOne.getTickets().add(ticketOne);
-//
-//                Ticket ticketTwo = new Ticket();
-//                ticketTwo.setCreatedAt(LocalDate.now());
-//                ticketTwo.setClient(clientOne);
-//                ticketTwo.setFromPlanet(planetNaboo);
-//                ticketTwo.setToPlanet(planetEndor);
-//                clientOne.getTickets().add(ticketTwo);
-//
-//                Ticket ticketThree = new Ticket();
-//                ticketThree.setCreatedAt(LocalDate.now());
-//                ticketThree.setClient(clientFive);
-//                ticketThree.setFromPlanet(planetEndor);
-//                ticketThree.setToPlanet(planetAlderaan);
-//                //clientFive.getTickets().add(ticketThree);
-//
-//                clientService.updateClient(clientOne);
-//                clientService.updateClient(clientFive);
-//
-//                List<Ticket> ticketsBeforeDelete = ticketCrudService.getAll();
-//                System.out.println("tickets = " + ticketsBeforeDelete.toString());
-//
-//                ticketCrudService.deleteById(3L);
-//                clientService.deleteClient(clientOne);
-//
-//                List<Ticket> ticketsAfterDelete = ticketCrudService.getAll();
-//                System.out.println("tickets = " + ticketsAfterDelete.toString());
+                ClientCrudService clientService = new ClientCrudService();
+                PlanetCrudService planetCrudService = new PlanetCrudService();
+                TicketCrudService ticketCrudService = new TicketCrudService();
+                Client clientOne = clientService.getClientById(1L);
+                Client clientFive = clientService.getClientById(5L);
+                Planet planetMars = planetCrudService.getPlanetById("MARS");
+                Planet planetVenus = planetCrudService.getPlanetById("VENUS");
+                Planet planetEarth = planetCrudService.getPlanetById("EARTH");
+
+                Ticket ticketOne = new Ticket();
+                ticketOne.setCreatedAt(LocalDate.now());
+                ticketOne.setClient(clientOne);
+                ticketOne.setFromPlanet(planetMars);
+                ticketOne.setFromPlanet(planetEarth);
+
+                clientOne.getTickets().add(ticketOne);
+
+                Ticket ticketTwo = new Ticket();
+                ticketTwo.setCreatedAt(LocalDate.now());
+                ticketTwo.setClient(clientOne);
+                ticketTwo.setFromPlanet(planetVenus);
+                ticketTwo.setToPlanet(planetMars);
+                clientOne.getTickets().add(ticketTwo);
+
+                Ticket ticketThree = new Ticket();
+                ticketThree.setCreatedAt(LocalDate.now());
+                ticketThree.setClient(clientFive);
+                ticketThree.setFromPlanet(planetEarth);
+                ticketThree.setToPlanet(planetVenus);
+                clientFive.getTickets().add(ticketThree);
+
+                clientService.updateClient(clientOne);
+                clientService.updateClient(clientFive);
+
+                List<Ticket> ticketsBeforeDelete = ticketCrudService.getAll();
+                System.out.println("tickets = " + ticketsBeforeDelete.toString());
+
+                ticketCrudService.deleteById(3L);
+                clientService.deleteClient(clientOne);
+
+                List<Ticket> ticketsAfterDelete = ticketCrudService.getAll();
+                System.out.println("tickets = " + ticketsAfterDelete.toString());
         }
 }
 
